7f777b4c6d20366d53e8e09a1cf5771523740b4a
RichardyPK <160357686+RichardyPK@users.noreply.github.com>
2025-09-12 08:05:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+function parte(){
+
+    var html = "";
+     html += "<img src='e69105d20802d3807493676a95f01de5.gif'></img>"
+     html += "<p>Você ligou para a menina, e ela atendeu, sai de motor?</p>";
+     html += '<div class="linha">'
+     html += '<button onclick="parte2()" style="width: 50%;">Sim!</button>';
+     html += '<button onclick="parte()" style="width: 50%;">Nao!</button>';
+     html += '</div>'
+
+    $( "body" ).append( html );
+
+}
+
+function parte2(){
+
+    var html = "";
+     html += "<img src='thriller-michael-jackson.gif '></img>";
+     html += "<p>Voces sairam e entraram numa loja, apocalipse zumbi!</p>";
+     html += '<div class="linha">'
+     html += '<button onclick="parte()" style="width: 50%;">Sim!</button>';
+     html += '<button onclick="parte()" style="width: 50%;">Nao!</button>';
+     html += '</div>'
+
+    $( "body" ).append( html );
+
+}
+
+
+function parte3(){
+
+    var html = "";
+     html += "<img src='a6738f68970401.5b70888321062.gif'></img>";
+     html += "<p>Você foi para a caverna, você medita e se eleva!</p>";
+     html += '<div class="linha">'
+     html += '<button onclick="parte()" style="width: 50%;">Sim!</button>';
+     html += '<button onclick="parte()" style="width: 50%;">Nao!</button>';
+     html += '</div>'
+
+    $( "body" ).append( html );
+
+}
+
+
